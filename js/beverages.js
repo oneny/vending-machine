045a4795b9e381014733b14cc6aee6f1eaf92a33
oneny <gjwodnjs1996@gmail.com>
2022-09-25 21:09:04 +0900

    Feat: 벤딩머신 섹션 음료수 리스트 만들기 완료

---
+++ js/beverages.js
@@ -0,0 +1,46 @@
+const beverages = [
+  {
+    id: "abcd1004",
+    name: "Original Cola",
+    price: 1000,
+    quantity: 10,
+    source: "./img/beverageImg/Original_Cola.png",
+  },
+  {
+    id: "bcds3294",
+    name: "Violet Cola",
+    price: 1000,
+    quantity: 0,
+    source: "./img/beverageImg/Violet_Cola.png",
+  },
+  {
+    id: "sfei9013",
+    name: "Violet Cola",
+    price: 1000,
+    quantity: 10,
+    source: "./img/beverageImg/Yellow_Cola.png",
+  },
+  {
+    id: "hdok3920",
+    name: "Violet Cola",
+    price: 1000,
+    quantity: 10,
+    source: "./img/beverageImg/Cool_Cola.png",
+  },
+  {
+    id: "npiq9284",
+    name: "Violet Cola",
+    price: 1000,
+    quantity: 10,
+    source: "./img/beverageImg/Green_Cola.png",
+  },
+  {
+    id: "zmit5821",
+    name: "Violet Cola",
+    price: 1000,
+    quantity: 10,
+    source: "./img/beverageImg/Orange_Cola.png",
+  },
+];
+
+export default beverages;
---
+++ js/main.js
@@ -0,0 +1,44 @@
+import beverages from "./beverages.js";
+
+const beveragesList = document.querySelector(".cont-lists");
+
+// 음료수 목록 만들기
+beverages.forEach(item => {
+  const beverageItem = document.createElement("li");
+  let count = 0;
+
+  // 아이디 숨겨서
+  const beverageId = document.createElement("input");
+  beverageId.setAttribute("type", "hidden");
+  beverageId.setAttribute("id", item["id"]);
+  beverageId.setAttribute("value", item["id"]);
+  beverageItem.appendChild(beverageId);
+  
+  // 수량 숨겨서
+  const beverageQuantity = document.createElement("input");
+  beverageQuantity.setAttribute("type", "hidden");
+  beverageQuantity.setAttribute("id", item["id"] + item["quantity"]);
+  beverageQuantity.setAttribute("value", item["quantity"]);
+  beverageItem.appendChild(beverageQuantity);
+
+  // 버튼 만들고 그 안에 이미지, 이름, 가격 넣기
+  const beverageBtn = document.createElement("button");
+
+  // 이미지
+  const beverageImg = document.createElement("img");
+  beverageImg.setAttribute("src", item["source"]);
+  beverageBtn.appendChild(beverageImg);
+
+  // 이름
+  const beverageName = document.createElement("strong");
+  beverageName.textContent = item["name"];
+  beverageBtn.appendChild(beverageName);
+
+  // 가격
+  const beveragePrice = document.createElement("span");
+  beveragePrice.textContent = item["price"];
+  beverageBtn.appendChild(beveragePrice);
+
+  beverageItem.appendChild(beverageBtn);
+  beveragesList.appendChild(beverageItem);
+});
